9b6acb544c0fcd0b2a10eb66be8f0c6cc1924877
kirtan-ciphernutz <kirtan@ciphernutz.com>
2026-04-10 16:43:17 +0530

    Explored RabbitMQ with RPC

---
+++ src/RPC/rpc_server.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import os
+
+import pika
+from dotenv import load_dotenv
+
+load_dotenv()
+
+RABBITMQ_URI = os.getenv("RABBITMQ_URI")
+
+if not RABBITMQ_URI:
+    raise RuntimeError("RABBITMQ_URI is not set")
+
+connection = pika.BlockingConnection(pika.URLParameters(RABBITMQ_URI))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='rpc_queue', durable=True, arguments={'x-queue-type': 'quorum'})
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+def on_request(ch, method, props, body):
+    n = int(body)
+
+    print(f" [.] fib({n})")
+    response = fib(n)
+
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id = \
+                                                         props.correlation_id),
+                     body=str(response))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='rpc_queue', on_message_callback=on_request)
+
+print(" [x] Awaiting RPC requests")
+channel.start_consuming()
